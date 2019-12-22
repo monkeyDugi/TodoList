074c7728ac2f22844e740708c0f92faa0ea124d8
monkeyDugi <lbd4946@gmail.com>
2019-12-22 22:03:30 +0900

    주석

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let close = document.getElementsByClassName("close");
-/**querySelector : 한가지 요소만 가져오며, 동일한 요소가 있으면 첫번째 요소만 가져온다. */
+/**querySelector : 한가지 요소만 가져오며, 동일한 요소가 있으면 첫번째 요소만 가져온다. 
+ * getElementsBy, getElementsBy : 배열 요소로 동일 한 모든 name을 가져오기 때문에 무조건 배열로 접근해야 한다. getElementsById는 별개임. id는 한개만 존재 가능하니까.
+ *
+ * ul태그가 한개만 존재하기 때문에 이럴 때는 querySelector를 사용하는 것도 나쁘지 않아 보인다. 
+*/
 let list = document.querySelector('ul');
 let li = document.createElement("li");
 let addBtn = document.getElementById("addBtn");
@@ -31,14 +35,10 @@ function addElement() {
 
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
-            var li = this.parentElement;
+            let li = this.parentElement;
             li.style.display = "none";
         }
     }    
-
-    console.log("listTag : " + listTag);
-    console.log("list : " + list);
-    
 }
 
 /**리스트 클릭 시 완료 표시 */
